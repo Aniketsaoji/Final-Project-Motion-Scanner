a36ae52be73ea136aa3ab432588b8cc0f93137e5
Aniket Saoji <saoji@bc.edu>
2016-04-26 18:13:38 -0400

    Small edits

--- web/public_html/motionsensor/index.php
+++ web/public_html/motionsensor/index.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<!--
+//@include '../../include/dbconn.php';
+//$dbc = connect_to_db('mitchko');
+//$var = perform_query_select($dbc, 'SELECT CONV(ORD(zones),10,2) AS Movement, motionTime AS `Time` FROM mitchko.motionHistory WHERE NOT (zones=b\'0000\') ORDER BY `motionTime` DESC;',array());
+//print json_encode($var);
+-->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,72 +15,33 @@
     <script src="js/main.js"></script>
     <link href="css/main.css" rel="stylesheet">
     <link href="css/jquery.dynatable.css" rel="stylesheet">
+	
+	<div class = dropdownn>
+		<ul class="nav nav-tabs">
+			<li role="presentation" class="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+					<h3 color:green>Services <span class="caret"></span> </h3>
+    </a>
+    	<ul class="dropdown-menu">
+			<li><a href = "test.html">Home</a></li>
+			<li><a href ="login.html">Login</a></li>
+			<li><a href ="quote.html">Get a Quote</a></li>
+			<li><a href ="help.html">Get Help</a></li>
+    	</ul>
+  </li>
+</ul>
+	</div>
 </head>
 <body>
-<div class="navbar navbar-default">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">62 Kirkwood</a>
-        </div>
+	<h1>Anti-Tickler Security</h1>
+	<p>
+	Lorem ipsum dolor sit amet, ne errem persius hendrerit pri, cu facilis eligendi indoctum pro, pro at fugit viris equidem. Ipsum numquam his in. Id mutat explicari mel, ea erroribus dissentiet vituperatoribus eam. Ea diam ceteros suscipit quo, vel ut percipit percipitur. Et duo maluisset constituam, quo putent erroribus ea. Eam cu scripta vulputate.
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Data
-                        <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#" data-toggle="modal" data-target="#myModal">Show XML Sources</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="page-header">
-            <h1>Movement</h1>
-        </div>
-        <div class="bs-component">
-            <table class="table table-bordered" id="movement">
-                <thead>
-                <tr>
-                <th data-dynatable-column="Movement">Movement</th>
-                <th data-dynatable-column="Time">Time</th>
-                </tr>
-                </thead>
-                <tbody></tbody>
-            </table>
-        </div>
-    </div>
-</div>
-<div class="modal" id="myModal">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Project Details</h4>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    <label class="col-lg-2 control-label">Intel Edison</label>
-                    <div class="col-lg-10">
-                        <a href="http://www.intel.com/content/www/us/en/do-it-yourself/edison.html">Project Page</a>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
+Quo iriure voluptatibus ut, quo magna dignissim id. Mutat laudem an sed. Ceteros ancillae nominavi per an, mea dicta oportere postulant ex. Nam et case reprimique, ad reque deleniti pro. Eam decore dolorum et, vero soleat omittam sit ne. Usu solum utamur et.
+
+Commodo atomorum delicatissimi ad vis. Velit ubique graece in eam, alterum aliquando repudiandae an pro. Atomorum iracundia ad vel, lorem possim fastidii ea has. Sea quis putent aliquip et. Fugit timeam epicurei ex nec, ei illud quaeque comprehensam vel. Usu et cibo contentiones.
+
+In vix numquam delectus, laudem deleniti qualisque no cum. Nullam latine sit in, ius pericula dignissim efficiendi ne, illum lorem interpretaris cum eu. At essent alterum bonorum vim. Nam audire impetus vituperata no, duo partem adversarium complectitur in. Dicit inermis id qui, at autem docendi voluptatum eos, ne vel vitae laoreet. Vidit iisque praesent sea no, pro at alii nemore feugiat. Deleniti appareat persequeris has ex.
+
+Eos at tota ancillae, at laudem nostrum est. Vix id mollis consectetuer. Liber errem graecis sea ad, in pro suas saperet. In nam movet necessitatibus, gubergren consetetur ea per. An mea illum tractatos.</p>
 </body>
-</html>
